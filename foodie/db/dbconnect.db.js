232c91ea477986ca39c5db09b729666c42c5816c
Hammad Hussain <hammadasmat@gmail.com>
2024-07-19 19:44:50 +0500

    db edited

--- foodie/db/dbconnect.db.js
+++ foodie/db/dbconnect.db.js
@@ -7,6 +7,9 @@ const url=process.env.URL;
 
 try {
    mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true });
+   if(mongoose.connection.readyState==1){
+       console.log("Database connected successfully");
+   };
 } catch (error) {
     console.log(error.message);
     
